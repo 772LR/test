f15412f34fdb8ae7841ca141800876cb451df731
B777 <B777@126.com>
2023-02-26 15:00:57 +0800

    222

--- src/test.java
+++ src/test.java
@@ -1,7 +1,7 @@
 public class test {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("hello");
-synchronized (this){
+synchronized (){
 
 }
 
