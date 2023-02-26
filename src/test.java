9cf58ade2148bb866b02c65776b2549736a1ecf9
B777 <B777@126.com>
2023-02-26 14:59:46 +0800

    222

--- src/test.java
+++ src/test.java
@@ -1,11 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello world");
-        System.out.println("pppx");
-        System.out.println();
-        System.out.println();
-        System.out.println();
+
+
     }
 }
